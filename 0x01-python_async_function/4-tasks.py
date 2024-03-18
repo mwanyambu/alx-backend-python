085432dcc5ae05e76214e7dd8d126cbd754a2ecf
mwanyasi <jmwanyambu@gmail.com>
2024-03-18 21:21:32 +0000

    pycodetyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,4 +11,4 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """span wait_random n times"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)] 
+    return [await task for task in asyncio.as_completed(tasks)]
