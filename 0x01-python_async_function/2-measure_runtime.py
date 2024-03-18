d36a518f0eab8e191b28044e020715a044f6c312
mwanyasi <jmwanyambu@gmail.com>
2024-03-18 21:14:32 +0000

    pycodestyle

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,6 +7,7 @@ from time import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """measure total execution time for waint_n"""
 
