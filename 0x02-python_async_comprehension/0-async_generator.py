e5869346d89507a0311084ce3220fb1aa8067147
mwanyasi <jmwanyambu@gmail.com>
2024-03-20 20:45:04 +0000

    pycodestyle

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """async generator"""
-import random
-import asyncio
+from asyncio import sleep
+from random import uniform
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     coroutine will loop 10 times asynchronously wait 1 sec and yeild random num
     """
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        await sleep(1)
+        yield uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 """async comprenhension"""
-
-import random
+from asyncio import sleep
+from random import uniform
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """
     coroutine will collect 10 random ints over async_generator
     """
